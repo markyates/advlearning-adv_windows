c6f385f9a5f7355de50b887045b1353fb5568227
Martin Reynolds <martin.reynolds@advancedcomputersoftware.com>
2014-11-04 23:01:05 +0000

    Syntax fix

--- spec/classes/msdtc_spec.rb
+++ spec/classes/msdtc_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe 'adv_windows::msdtc', :type => class do
+describe 'adv_windows::msdtc', :type => 'class' do
   it 'should update msdtc settings' do
     should contain_exec('Configure MSDTC')
   end
