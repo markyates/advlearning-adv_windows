4dca9e62eade05562a7a83dbdf26a6a9d9c5e2e4
Martin Reynolds <martin.reynolds@advancedcomputersoftware.com>
2014-11-07 22:03:40 +0000

    Fixes to tests to match bug fixes in associated manifest

--- spec/classes/centrastage_spec.rb
+++ spec/classes/centrastage_spec.rb
@@ -5,7 +5,7 @@ describe 'adv_windows::centrastage', :type => 'class' do
                   :csenv => 'Test'} }
 
   it 'should copy installer file' do
-    should contain_file('CSInstaller-Test').with({
+    should contain_file('CSInstaller').with({
       'ensure' => 'present',
       'path'   => 'D:\\Software\\AgentSetup_Test.exe',
       'source' => 'puppet:///modules/adv_windows/centrastage/AgentSetup_Test.exe'
@@ -13,7 +13,7 @@ describe 'adv_windows::centrastage', :type => 'class' do
   end
 
   it 'should run the install' do
-    should contain_exec('CSInstall-Test').with({
+    should contain_exec('CSInstall').with({
       'command' => 'D:\\Software\\AgentSetup_Test.exe',
       'creates' => 'C:\\Program Files (x86)\\CentraStage'
     })
