0a7223c30560acc47fe2144b14df6963e1de5f13
Martin Reynolds <martin.reynolds@advancedcomputersoftware.com>
2015-05-23 14:41:32 +0100

    cloudhealth class

---
+++ manifests/cloudheath.pp
@@ -0,0 +1,19 @@
+# manifests/cloudhealth.pp
+
+class adv_windows::cloudhealth($workFolder) {
+  File { source_permissions => ignore }
+  ensure_resource(file, $workFolder, { ensure => directory })
+
+  file{'BitdefenderInstaller':
+    ensure => present,
+    path   => "${workFolder}\\CloudHealthAgent.exe",
+    source => 'puppet:///modules/adv_windows/CloudHealthAgent.exe',
+    notify => Exec['CloudHealthInstall']
+  }
+
+  exec{'CloudHealthInstall':
+    command     => "${workFolder}\\CloudHealthAgent.exe /S /v""/l* install.log /qn CLOUDNAME=aws CHTAPIKEY=b8749822-2fb1-4292-b4e6-8c35fbcb5e41""",
+    refreshonly => true
+  }
+
+}
