213256c76de8a7f8a54c084693b34d6c1c2f464c
Martin Reynolds <martin.reynolds@advancedcomputersoftware.com>
2015-05-23 14:56:00 +0100

    escaping fix

--- manifests/cloudhealth.pp
+++ manifests/cloudhealth.pp
@@ -12,7 +12,7 @@ class adv_windows::cloudhealth($workFolder) {
   }
 
   exec{'CloudHealthInstall':
-    command     => "${workFolder}\\CloudHealthAgent.exe /S /v""/l* install.log /qn CLOUDNAME=aws CHTAPIKEY=b8749822-2fb1-4292-b4e6-8c35fbcb5e41""",
+    command     => "${workFolder}\\CloudHealthAgent.exe /S /v\"/l* install.log /qn CLOUDNAME=aws CHTAPIKEY=b8749822-2fb1-4292-b4e6-8c35fbcb5e41\"",
     refreshonly => true
   }
 
