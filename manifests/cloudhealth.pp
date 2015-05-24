c44dad2754d720d0d934efd2971705fe476cc633
Martin Reynolds <martin.reynolds@advancedcomputersoftware.com>
2015-05-24 13:40:23 +0100

    updated to run if not installed rather than on notifier

--- manifests/cloudhealth.pp
+++ manifests/cloudhealth.pp
@@ -7,13 +7,13 @@ class adv_windows::cloudhealth($workFolder) {
   file{'CloudHealthInstaller':
     ensure => present,
     path   => "${workFolder}\\CloudHealthAgent.exe",
-    source => 'puppet:///modules/adv_windows/CloudHealthAgent.exe',
-    notify => Exec['CloudHealthInstall']
+    source => 'puppet:///modules/adv_windows/CloudHealthAgent.exe'
   }
 
   exec{'CloudHealthInstall':
-    command     => "${workFolder}\\CloudHealthAgent.exe /S /v\"/l* install.log /qn CLOUDNAME=aws CHTAPIKEY=b8749822-2fb1-4292-b4e6-8c35fbcb5e41\"",
-    refreshonly => true
+    command => "${workFolder}\\CloudHealthAgent.exe /S /v\"/l* install.log /qn CLOUDNAME=aws CHTAPIKEY=b8749822-2fb1-4292-b4e6-8c35fbcb5e41\"",
+    creates => 'C:\Program Files (x86)\CloudHealth Technologies\CloudHealth Agent'
+    requires => File['CloudHealthInstaller']
   }
 
 }
