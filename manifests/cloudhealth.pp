1886e9cc1d9798367a29c002604af2a99d419933
Martin Reynolds <martin.reynolds@advancedcomputersoftware.com>
2015-05-24 18:37:04 +0100

    linting

--- manifests/cloudhealth.pp
+++ manifests/cloudhealth.pp
@@ -13,7 +13,7 @@ class adv_windows::cloudhealth($workFolder) {
   exec{'CloudHealthInstall':
     command  => 'CloudHealthAgent.exe /S /v"/l* install.log /qn CLOUDNAME=aws CHTAPIKEY=b8749822-2fb1-4292-b4e6-8c35fbcb5e41"',
     cwd      => "${workFolder}\\",
-    creates  => 'C:\Program Files (x86)\CloudHealth Technologies\CloudHealth Agent'
+    creates  => 'C:\Program Files (x86)\CloudHealth Technologies\CloudHealth Agent',
     requires => File['CloudHealthInstaller']
   }
 
