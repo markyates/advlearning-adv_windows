82cfff9c1aa01653971595311806348cee73d8e7
Martin Reynolds <martin.reynolds@advancedcomputersoftware.com>
2015-05-24 18:25:41 +0100

    changed to set a work folder

--- manifests/cloudhealth.pp
+++ manifests/cloudhealth.pp
@@ -11,8 +11,9 @@ class adv_windows::cloudhealth($workFolder) {
   }
 
   exec{'CloudHealthInstall':
-    command => "${workFolder}\\CloudHealthAgent.exe /S /v\"/l* install.log /qn CLOUDNAME=aws CHTAPIKEY=b8749822-2fb1-4292-b4e6-8c35fbcb5e41\"",
-    creates => 'C:\Program Files (x86)\CloudHealth Technologies\CloudHealth Agent'
+    command  => 'CloudHealthAgent.exe /S /v"/l* install.log /qn CLOUDNAME=aws CHTAPIKEY=b8749822-2fb1-4292-b4e6-8c35fbcb5e41"',
+    cwd      => "${workFolder}\\"
+    creates  => 'C:\Program Files (x86)\CloudHealth Technologies\CloudHealth Agent'
     requires => File['CloudHealthInstaller']
   }
 
