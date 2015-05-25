a7b599282ea7389861405fb64453563386657418
Martin Reynolds <martin.reynolds@advancedcomputersoftware.com>
2015-05-25 17:44:19 +0100

    ?

--- manifests/cloudhealth.pp
+++ manifests/cloudhealth.pp
@@ -11,8 +11,7 @@ class adv_windows::cloudhealth($workFolder) {
   }
 
   exec{'CloudHealthInstall':
-    command => 'CloudHealthAgent.exe /S /v"/l* install.log /qn CLOUDNAME=aws CHTAPIKEY=b8749822-2fb1-4292-b4e6-8c35fbcb5e41"',
-    cwd     => "${workFolder}\\",
+    command => "${workFolder}\\CloudHealthAgent.exe /S /v\"/l* install.log /qn CLOUDNAME=aws CHTAPIKEY=b8749822-2fb1-4292-b4e6-8c35fbcb5e41\"",
     creates => 'C:\Program Files (x86)\CloudHealth Technologies\CloudHealth Agent',
     require => File['CloudHealthInstaller']
   }
