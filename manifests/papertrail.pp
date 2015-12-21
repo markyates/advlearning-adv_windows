9c3ff09edcf5e25b963d2ef6ca816542e75c5ccf
Martin Reynolds <martin.reynolds@advancedcomputersoftware.com>
2015-12-21 06:32:15 +0000

    Papertrail (nxlog) changed to stop running so no submitting data

--- manifests/papertrail.pp
+++ manifests/papertrail.pp
@@ -3,24 +3,24 @@
 class adv_windows::papertrail($host,
                               $port) {
 
-  package{'nxlog':
-    ensure   => latest,
-    provider => 'chocolatey'
-  }
+#  package{'nxlog':
+#    ensure   => latest,
+#    provider => 'chocolatey'
+#  }
 
-  ensure_resource(file, 'C:\\Program Files (x86)\\nxlog', {ensure => directory})
-  ensure_resource(file, 'C:\\Program Files (x86)\\nxlog\\conf', {ensure => directory})  # lint:ignore:80chars
+#  ensure_resource(file, 'C:\\Program Files (x86)\\nxlog', {ensure => directory})
+#  ensure_resource(file, 'C:\\Program Files (x86)\\nxlog\\conf', {ensure => directory})  # lint:ignore:80chars
 
-  file{'nxlog.conf':
-    ensure  => present,
-    path    => 'C:\Program Files (x86)\nxlog\conf\nxlog.conf',
-    content => template('adv_windows/nxlog.conf.erb'),
-    require => Package['nxlog']
-  }
+#  file{'nxlog.conf':
+#    ensure  => present,
+#    path    => 'C:\Program Files (x86)\nxlog\conf\nxlog.conf',
+#    content => template('adv_windows/nxlog.conf.erb'),
+#    require => Package['nxlog']
+#  }
 
   service{'nxlog':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nxlog']
+    ensure  => 'stopped',
+    enable  => 'manual'
+#    require => Package['nxlog']
   }
 }
